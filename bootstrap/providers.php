28fe0b225cf9322297b9dd4c325fbe8869cbd43f
Nchandom Alpha Elias <nchandoms@gmail.com>
2026-06-27 12:09:42 +0100

    Landing page, user portal, and admin portal added

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-
 return [
-    AppServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\HomePanelProvider::class,
+    App\Providers\Filament\UserPanelProvider::class,
 ];
